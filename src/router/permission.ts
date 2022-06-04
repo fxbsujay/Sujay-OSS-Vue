bb301f2589f87adc97c75e61f51a9d9a941d9f91
fxbsujay <fxbsujay@gmail.com>
2022-06-05 00:46:48 +0800

    页面加载进度条

--- src/router/permission.ts
+++ src/router/permission.ts
@@ -1,26 +1,43 @@
 import router from './index'
 import store from "../store";
 import { getToken } from '../utils/cookies'
-const whiteList: Array<string> = ['/login']
-
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 /**
- * @description 快前置由守卫
+ * @description 路由守卫
  * @author fxbsujay@gmail.com
  * @version 0:28 2022/6/5
  */
+
+const whiteList: Array<string> = ['/login']
+
+/**
+ * 加载条
+ */
+NProgress.configure({ showSpinner: false })
+
 router.beforeEach( (to, from, next)=> {
+    NProgress.start()
     const token:string = store.getters.token ||  getToken()
     if (!token) {
         if (whiteList.includes(to.path)) {
             next()
+            NProgress.done()
         } else {
             next('/login')
+            NProgress.done()
         }
     } else {
         if (to.path === '/login') {
             next('/')
+            NProgress.done()
         } else {
             next()
+            NProgress.done()
         }
     }
+})
+
+router.afterEach((to) => {
+    NProgress.done()
 })
